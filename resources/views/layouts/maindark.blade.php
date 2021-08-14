6505bb0855828008607c5f8470bc496bde2c162d
Poe Ti <pugna1102@gmail.com>
2021-08-14 14:09:14 +0630

    Favicon added

--- resources/views/layouts/maindark.blade.php
+++ resources/views/layouts/maindark.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ url('images/favicon.png') }}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
